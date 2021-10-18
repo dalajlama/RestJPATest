c77f6e76398473308d390f6cb97742829c4f7be1
Oliver <oliver.jelenic@fina.hr>
2021-10-18 13:25:58 +0200

    Dodan createdAt and updatedAt

--- src/main/java/com/example/restjpatest/DTO/Zaposlenik.java
+++ src/main/java/com/example/restjpatest/DTO/Zaposlenik.java
@@ -6,8 +6,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.Hibernate;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.Objects;
 
 @Getter
@@ -21,32 +24,42 @@ public class Zaposlenik {
     @Id
     // ovo govori da se ID generira automatski
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
+    // imena kolona nisu potrebna osim ako želimo mapirati na neko drugo ime
+    // @Column(name="id")
     private Long id;
 
-    //@JsonProperty mapira navedeno(puno_ime) u ime
-    //onda se u requestu mora poslati puno_ime umjesto ime
+    // @JsonProperty mapira navedeno(puno_ime) u ime
+    // onda se u requestu mora poslati puno_ime umjesto ime
+    // Imena kolona nisu potrebna ukoliko su imena u tablici ista kao varijabla
     @JsonProperty("puno_ime")
-    @Column(name = "ime")
+    // @Column(name = "ime")
     private String ime;
 
-    @Column(name = "prezime")
+    // @Column(name = "prezime")
     private String prezime;
 
     //@JsonIgnore sakriva određeni field u responsu koji se vraća korisniku
     // isto tako ga zanemaruje i prilikom updatea
     // @JsonIgnore
-    @Column(name = "godine")
+    // @Column(name = "godine")
     private Long godine;
 
-    @Column(name = "lokacija")
+    // @Column(name = "lokacija")
     private String lokacija;
 
-    @Column(name = "email")
+    // @Column(name = "email")
     private String email;
 
-    @Column(name = "odjel")
+    // @Column(name = "odjel")
     private String odjel;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
 
     @Override
     public boolean equals(Object o) {
--- src/main/java/com/example/restjpatest/service/ZaposlenikServiceImpl.java
+++ src/main/java/com/example/restjpatest/service/ZaposlenikServiceImpl.java
@@ -3,11 +3,13 @@ package com.example.restjpatest.service;
 import com.example.restjpatest.DTO.Zaposlenik;
 import com.example.restjpatest.repository.ZaposlenikRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class ZaposlenikServiceImpl implements ZaposlenikService{
@@ -24,32 +26,32 @@ public class ZaposlenikServiceImpl implements ZaposlenikService{
 
     @Override
     public Optional<Zaposlenik> dohvatiZaposlenika(Long id) {
-        System.out.println("Dohvaćam zaposlenika  id: " + id);
+        log.info("Dohvaćam zaposlenika  id: {}",id );
         return zaposlenikRepository.findById(id);
     }
 
     @Override
     public List<Zaposlenik> dohvatiSveZaposlenike() {
-        System.out.println("Dohvati sve zaposlenike");
+        log.info("Dohvati sve zaposlenike");
         return zaposlenikRepository.findAll();
     }
 
     @Override
     public Zaposlenik spremiZaposlenika(Zaposlenik zaposlenik) {
-        System.out.println("Spremam detalje u bazu za zaposlenika: " + zaposlenik.getIme());
+        log.info("Spremam detalje u bazu za zaposlenika: {} ", zaposlenik.getIme());
         return zaposlenikRepository.save(zaposlenik);
     }
 
     @Override
     public String obrisiZaposlenika(Long id) {
-        System.out.println("Brišem zaposlenika id: " + id);
+        log.info("Brišem zaposlenika id: {} ", id);
         zaposlenikRepository.deleteById(id);
         return "Zaposlenik " + id + " obrisan!";
     }
 
     @Override
     public Zaposlenik azurirajZaposlenika(Zaposlenik zaposlenik) {
-        System.out.println("Vršim update Zaposlenika: " + zaposlenik.getIme() + " pod id: " + zaposlenik.getId());
+        log.info("Vršim update Zaposlenika: {}  pod id:  {}", zaposlenik.getIme(), zaposlenik.getId());
         return zaposlenikRepository.save(zaposlenik);
     }
 }
